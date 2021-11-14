85e2e08d79a2cabcac5f7967e7bd09fbff8dfc0f
Patricia Freitas <patricia.freitas@ticto.com.br>
2021-11-14 18:52:22 -0300

    Ongs_Youtube

---
+++ cypress/support/commands.js
@@ -0,0 +1,43 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("createOng", () => {
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/ongs',
+        body: {                
+                city: "Americana",
+                email: "cadm@teste.com",
+                name: "Gatos queridos",
+                uf: "Sp",
+                whatsapp: "19999988877"
+        }
+    }).then(response => {
+        expect(response.body.id).is.not.null;
+        cy.log(response.body.id);
+        Cypress.env('createdOngId', response.body.id);
+    });
+});
