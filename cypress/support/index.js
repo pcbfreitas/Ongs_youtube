4dc34e9beab1d43b606db4aab79c635c75960480
Patricia Freitas <patricia.freitas@ticto.com.br>
2021-11-16 18:20:05 -0300

    alinhamento

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -4,10 +4,10 @@ before(() => {
     // cy.server() - para route e request
     cy.server();
     //variaveis env
-    var name =Cypress.env('name', 'Gatos queridos');
-    var email = Cypress.env('email', 'teste@email.com');
+    var name =     Cypress.env('name', 'Gatos queridos');
+    var email =    Cypress.env('email', 'teste@email.com');
     var whatsapp = Cypress.env('whatsapp', '1999999999');
-    var city = Cypress.env('city', 'Campinas');
-    var uf = Cypress.env('uf', 'SP'); 
+    var city =     Cypress.env('city', 'Campinas');
+    var uf =       Cypress.env('uf', 'SP'); 
     cy.createOng(name, email, whatsapp, city, uf);
 });
